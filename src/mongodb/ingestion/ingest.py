ffd40c6e7598cf275907f98c95574237f40129ac
nchenche <nicolas.chevrollier@laposte.net>
2025-01-06 11:28:05 +0100

    clean comments

--- src/mongodb/ingestion/ingest.py
+++ src/mongodb/ingestion/ingest.py
@@ -54,36 +54,6 @@ def ingest_data_to_mongodb(source: str|Path):
         db["global_properties"].drop()
         db["global_properties"].insert_many(mongodb_collections["properties"])
 
-
-        # 2. Clean and transform data
-        # df_clean = clean_dataframe(df)
-        # logger.info('Step 2 completed: Dataframe cleaned and transformed.')
-
-        
-        # # 3. Serialize for MongoDB
-        # mongodb_collections = serialize_to_mongodb_collections(df_clean)
-        # logger.info('Step 3 completed: Data collections ready to be ingested.')
-        
-        # # 4. Access and insert data to the db
-        # db = get_db()
-        # logger.info(f'Step 4.1 completed: Successful access to the database {db.name}.')
-
-        # monomers_collection = db["global_monomers"]
-        # properties_collection = db["global_properties"]
-        # sdf_collections = db["global_sdf"]
-
-        # monomers_collection.drop()
-        # monomers_collection.insert_many(mongodb_collections["monomers"])
-        # logger.info(f'Step 4.2 completed: monomers data successfully added as "global_monomers" collection.')
-
-        # properties_collection.drop()
-        # properties_collection.insert_many(mongodb_collections["properties"])
-        # logger.info(f'Step 4.3 completed: properties data successfully added as "global_properties" collection.')
-        
-        # sdf_collections.drop()
-        # sdf_collections.insert_many(mongodb_collections["sdf"])
-        # logger.info(f'Step 4.4 completed: sdf data successfully added as "global_sdf" collection.')
-
         logger.info('Ingestion completed.')
     except Exception as e:
         logger.error(f"Ingestion failed: {e}")
